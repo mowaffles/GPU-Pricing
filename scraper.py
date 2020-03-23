90fb17649081b08672d8e39ff47d70f865dacd5c
Oli <oliversimmons761@gmail.com>
2020-03-23 00:46:28 +0000

    Add CSV functionality

--- scraper.py
+++ scraper.py
@@ -7,12 +7,47 @@ import requests
 from bs4 import BeautifulSoup
 import matplotlib.pyplot as plt
 from matplotlib import style
+import csv
 
 item_name = []
 prices = []
 dates = []
 
-def scaper(urlObj):
+def writeCSV(filename):
+	global item_name
+	global prices
+	global dates
+	with open(filename, mode='w') as csv_file:
+		csv_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+		
+		csv_writer.writerow(['Name', 'Price', 'Date'])	#Write header row
+		for i in range(0, len(item_name)):
+			csv_writer.writerow([item_name[i], prices[i], dates[i] ])
+
+def readCSV(filename):
+	global item_name
+	global prices
+	global dates
+	with open(filename) as csv_file:
+		print("Reading CSV...")
+		csv_reader = csv.reader(csv_file)
+		line_count = 0
+		for row in csv_reader:
+			if line_count == 0:
+				#print(f'Column names are {", ".join(row)}')
+				line_count += 1
+			else:
+				item_name.append(row[0])
+				prices.append(float(row[1]))
+				dates.append(row[2])
+				#print(f'\t{row[0]} : {row[1]} : {row[2]}.')
+				line_count += 1
+		print(f'Processed {line_count} lines.')
+
+def scraper(urlObj):
+	global item_name
+	global prices
+	global dates
 	for i in range(1,(urlObj[1] + 1)):
 		ebayUrl = urlObj[0] + str(i)
 		r= requests.get(ebayUrl)
@@ -46,7 +81,15 @@ def scaper(urlObj):
 	for i in range(0, len(item_name)): #Raw output
 		print("Item: ", i, " | Name: ", item_name[i], " | Price: ", prices[i],  " | Date: ", dates[i])
 
+	#reverse all lists
+	item_name = list(reversed(item_name))
+	prices = list(reversed(prices))
+	dates = list(reversed(dates))
+
 def removeTi():
+	global item_name
+	global prices
+	global dates
 	#Remove 1080Ti entries
 	toDelete = []
 	for i in range(0, len(item_name)):
@@ -63,18 +106,17 @@ def graphing():
 	global item_name
 	global prices
 	global dates
-	#reverse all lists
-	item_name = list(reversed(item_name))
-	prices = list(reversed(prices))
-	dates = list(reversed(dates))
 
 	#Graphing
 	style.use('ggplot')
-
+	
+	plt.subplot(211)
 	plt.plot(prices)
 	plt.xlabel("Date")
 	plt.ylabel("Price (£)")
 
+	plt.subplot(212)
+	plt.boxplot(prices, vert=False)
 	plt.show()
 
 def printData():
@@ -86,7 +128,9 @@ GTX1080url = ["https://www.ebay.co.uk/b/NVIDIA-GeForce-GTX-1080-Computer-Graphic
 GTX1070url = ["https://www.ebay.co.uk/b/NVIDIA-GeForce-GTX-1070-NVIDIA-Computer-Graphics-Video-Cards/27386/bn_100736487?LH_ItemCondition=3000&LH_Sold=1&rt=nc&_sop=13&_pgn=", 6]
 RTX2060url = ["https://www.ebay.co.uk/b/Industrial-NVIDIA-GeForce-RTX-2060-Computer-Graphics-Video-Cards/27386/bn_7116613730?LH_ItemCondition=3000&LH_Sold=1&rt=nc&_sop=13&_pgn=", 2]
 
-scaper(GTX1070url)
-#removeTi()
+#scraper(GTX1070url)
+readCSV("GTX1070.csv")
+removeTi()
 printData()
+#writeCSV("GTX1070.csv")
 graphing()
