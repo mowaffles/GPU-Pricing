737bb1753ca31889754a97fa2207829db59166c5
Oli <oliversimmons761@gmail.com>
2020-03-23 00:05:49 +0000

    Convert to functions

--- scaper.py
+++ scaper.py
@@ -12,50 +12,45 @@ item_name = []
 prices = []
 dates = []
 
-
-for i in range(1,7):
-	GTX1080url = "https://www.ebay.co.uk/b/NVIDIA-GeForce-GTX-1080-Computer-Graphics-Video-Cards/27386/bn_100742202?LH_ItemCondition=3000&LH_Sold=1&rt=nc&_pgn="+str(i)+"&_sop=13&_udlo=100"
-	GTX1070url = "https://www.ebay.co.uk/b/NVIDIA-GeForce-GTX-1070-NVIDIA-Computer-Graphics-Video-Cards/27386/bn_100736487?LH_ItemCondition=3000&LH_Sold=1&rt=nc&_pgn="+str(i)+"&_sop=13"
-
-	ebayUrl = GTX1070url
-	r= requests.get(ebayUrl)
-	data=r.text
-	soup=BeautifulSoup(data)
- 
-	listings = soup.find_all('li', attrs={'class': 's-item'})
- 
-	for listing in listings:
-		prod_name=" "
-		prod_price = " "
-		for name in listing.find_all('h3', attrs={'class':"s-item__title"}):
-			if(str(name.find(text=True, recursive=False))!="None"):
-				prod_name=str(name.find(text=True, recursive=False))
-				item_name.append(prod_name)
- 
-		if(prod_name!=" "):
-			price = listing.find('span', attrs={'class':"s-item__price"})
-			prod_price = price.get_text()
-			prod_price = prod_price.replace("£","")
-			prod_price = float(prod_price)
-			prices.append(prod_price)
-
-		if(prod_name!=" "):
-			date = listing.find('span', attrs={'class':"s-item__ended-date"})
-			prod_date = date.get_text()
-			dates.append(prod_date)
+def scaper(urlObj):
+	for i in range(1,(urlObj[1] + 1)):
+		ebayUrl = urlObj[0] + str(i)
+		r= requests.get(ebayUrl)
+		data=r.text
+		soup=BeautifulSoup(data)
 	
-	print("Current page:",i)
+		listings = soup.find_all('li', attrs={'class': 's-item'})
+	
+		for listing in listings:
+			prod_name=" "
+			prod_price = " "
+			for name in listing.find_all('h3', attrs={'class':"s-item__title"}):
+				if(str(name.find(text=True, recursive=False))!="None"):
+					prod_name=str(name.find(text=True, recursive=False))
+					item_name.append(prod_name)
+	
+			if(prod_name!=" "):
+				price = listing.find('span', attrs={'class':"s-item__price"})
+				prod_price = price.get_text()
+				prod_price = prod_price.replace("£","")
+				prod_price = float(prod_price)
+				prices.append(prod_price)
 
-for i in range(0, len(item_name)): #Raw output
-	print("Item: ", i, " | Name: ", item_name[i], " | Price: ", prices[i],  " | Date: ", dates[i])
+			if(prod_name!=" "):
+				date = listing.find('span', attrs={'class':"s-item__ended-date"})
+				prod_date = date.get_text()
+				dates.append(prod_date)
+		
+		print("Current page:",i)
 
-
+	for i in range(0, len(item_name)): #Raw output
+		print("Item: ", i, " | Name: ", item_name[i], " | Price: ", prices[i],  " | Date: ", dates[i])
 
 def removeTi():
 	#Remove 1080Ti entries
 	toDelete = []
 	for i in range(0, len(item_name)):
-		if ( (item_name[i]).find("Ti") != -1):
+		if ( ((item_name[i]).find("Ti") != -1) or ((item_name[i]).find("TI ") != -1) ):
 			#print(item_name[i])
 			toDelete.insert(0,i)
 	#toDelete = list(reversed(toDelete))	#Flip to avoid
@@ -64,18 +59,34 @@ def removeTi():
 		del prices[elem]
 		del dates[elem]
 
-removeTi()
+def graphing():
+	global item_name
+	global prices
+	global dates
+	#reverse all lists
+	item_name = list(reversed(item_name))
+	prices = list(reversed(prices))
+	dates = list(reversed(dates))
 
-output = pd.DataFrame({"Name":item_name, "Prices": prices, "Date": dates})
-print (output)
+	#Graphing
+	style.use('ggplot')
 
-#reverse all lists
-item_name = list(reversed(item_name))
-prices = list(reversed(prices))
-dates = list(reversed(dates))
+	plt.plot(prices)
+	plt.xlabel("Date")
+	plt.ylabel("Price (£)")
 
-#Graphing
-style.use('ggplot')
+	plt.show()
 
-plt.plot(prices)
-plt.show()
+def printData():
+	output = pd.DataFrame({"Name":item_name, "Prices": prices, "Date": dates})
+	pd.set_option('display.max_rows', len(item_name))
+	print (output)
+
+GTX1080url = ["https://www.ebay.co.uk/b/NVIDIA-GeForce-GTX-1080-Computer-Graphics-Video-Cards/27386/bn_100742202?LH_ItemCondition=3000&LH_Sold=1&rt=nc&_sop=13&_pgn=", 4]
+GTX1070url = ["https://www.ebay.co.uk/b/NVIDIA-GeForce-GTX-1070-NVIDIA-Computer-Graphics-Video-Cards/27386/bn_100736487?LH_ItemCondition=3000&LH_Sold=1&rt=nc&_sop=13&_pgn=", 6]
+RTX2060url = ["https://www.ebay.co.uk/b/Industrial-NVIDIA-GeForce-RTX-2060-Computer-Graphics-Video-Cards/27386/bn_7116613730?LH_ItemCondition=3000&LH_Sold=1&rt=nc&_sop=13&_pgn=", 2]
+
+scaper(GTX1070url)
+#removeTi()
+printData()
+graphing()
